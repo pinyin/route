4343ed4596036f9706a39e77ed51f68f84eae9cb
pinyin <bob432091@gmail.com>
2018-07-05 21:30:28 +0800

    export pattern types

--- index.ts
+++ index.ts
@@ -1,2 +1,4 @@
 export {createDeserializer} from './src/createDeserializer'
 export {createSerializer} from './src/createSerializer'
+export {SegmentPattern} from './src/SegmentPattern'
+export {QueryPattern} from './src/QueryPattern'
